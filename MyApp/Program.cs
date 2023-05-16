ddb359c17d1237f77bb684a506ae3a00c6aa37b8
murilodr13 <54368190+murilodr13@users.noreply.github.com>
2023-05-16 20:15:02 -0300

    Update Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace MeuProjeto.Tests
+{
+    [TestFixture]
+    public class MeuProjetoTests
+    {
+        [Test]
+        public void TestHelloWorld()
+        {
+            // Arrange
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            Program.Main(null);
+            string output = stringWriter.ToString().Trim();
+
+            // Assert
+            Assert.AreEqual("Hello, World!", output);
+        }
+
+        [Test]
+        public void TestArtifactGeneration()
+        {
+            // Arrange
+            string artifactPath = Path.Combine(Environment.CurrentDirectory, "artifacts");
+            string zipPath = Path.Combine(artifactPath, "MeuProjeto.zip");
+
+            // Act
+            Program.Main(null);
+
+            // Assert
+            Assert.IsTrue(File.Exists(zipPath));
+        }
+    }
+}
