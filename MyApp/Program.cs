fa5f21556313503b3b651b8ed895f394a65c294e
Murilo Damas Rodrigues <murilo.damas@ituran.com.br>
2025-06-03 12:40:36 -0300

    Fix project and adding jenkinsfile

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,40 +1,53 @@
-using NUnit.Framework;
+// using NUnit.Framework;
+// using System;
+// using System.IO;
+// using System.IO.Compression;
+
+// namespace MeuProjeto.Tests
+// {
+//     [TestFixture]
+//     public class MeuProjetoTests
+//     {
+//         [Test]
+//         public void TestHelloWorld()
+//         {
+//             // Arrange
+//             StringWriter stringWriter = new StringWriter();
+//             Console.SetOut(stringWriter);
+
+//             // Act
+//             Program.Main(null);
+//             string output = stringWriter.ToString().Trim();
+
+//             // Assert
+//             Assert.AreEqual("Hello, World!", output);
+//         }
+
+//         [Test]
+//         public void TestArtifactGeneration()
+//         {
+//             // Arrange
+//             string artifactPath = Path.Combine(Environment.CurrentDirectory, "artifacts");
+//             string zipPath = Path.Combine(artifactPath, "MeuProjeto.zip");
+
+//             // Act
+//             Program.Main(null);
+
+//             // Assert
+//             Assert.IsTrue(File.Exists(zipPath));
+//         }
+//     }
+// }
+
 using System;
-using System.IO;
-using System.IO.Compression;
 
-namespace MeuProjeto.Tests
+namespace dotnet_test_old
 {
-    [TestFixture]
-    public class MeuProjetoTests
+    class Program
     {
-        [Test]
-        public void TestHelloWorld()
+        static void Main(string[] args)
         {
-            // Arrange
-            StringWriter stringWriter = new StringWriter();
-            Console.SetOut(stringWriter);
-
-            // Act
-            Program.Main(null);
-            string output = stringWriter.ToString().Trim();
-
-            // Assert
-            Assert.AreEqual("Hello, World!", output);
-        }
-
-        [Test]
-        public void TestArtifactGeneration()
-        {
-            // Arrange
-            string artifactPath = Path.Combine(Environment.CurrentDirectory, "artifacts");
-            string zipPath = Path.Combine(artifactPath, "MeuProjeto.zip");
-
-            // Act
-            Program.Main(null);
-
-            // Assert
-            Assert.IsTrue(File.Exists(zipPath));
+            Console.WriteLine("Hello World");
         }
     }
 }
